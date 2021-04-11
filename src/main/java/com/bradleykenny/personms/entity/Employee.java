c4aac359b4788ba0b1241f42ea52833ff2c4f749
Bradley Kenny <brad.kenny@swipejobs.com>
2021-04-11 18:28:47 +1000

    Basic Employee object and changes to responses.

---
+++ src/main/java/com/bradleykenny/personms/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.bradleykenny.personms.entity;
+
+import java.util.Date;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
--- src/main/java/com/bradleykenny/personms/rest/External.java
+++ src/main/java/com/bradleykenny/personms/rest/External.java
@@ -2,15 +2,24 @@ package com.bradleykenny.personms.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/external/employees")
 public class External {
 
     @GetMapping
-    public String getEmployees() {
-        return "Hello, world!";
+    public ResponseEntity<String> getEmployees() {
+        return ResponseEntity.ok("Hello, world!");
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postEmployees() {
+        return ResponseEntity.status(200).build();
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<String> getAllEmployees() {
+        return ResponseEntity.ok("Hello, all!");
     }
 
 }
