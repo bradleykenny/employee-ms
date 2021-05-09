b1c8cd0618e74f1298f1a82cb6951832fb38cd8b
Bradley Kenny <brad.kenny@swipejobs.com>
2021-05-09 22:17:47 +1000

    Config added to hidden file.

--- src/main/java/com/bradleykenny/personms/config/MongoConfig.java
+++ src/main/java/com/bradleykenny/personms/config/MongoConfig.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Configuration
+@PropertySource("classpath:mongo.properties")
 public class MongoConfig {
 
     @Value("${mongo.url}")
