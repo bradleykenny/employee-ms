fb828e6f94cfe92b2cccc3528e6d7623fdaa4aaf
Bradley Kenny <brad.kenny@swipejobs.com>
2021-05-09 22:00:41 +1000

    Committing config file.

--- src/main/java/com/bradleykenny/personms/config/MongoConfig.java
+++ src/main/java/com/bradleykenny/personms/config/MongoConfig.java
@@ -1,14 +1,16 @@
-package com.bradleykenny.personms.database;
+package com.bradleykenny.personms.config;
 
 import com.mongodb.*;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MongoDB {
+@Configuration
+public class MongoConfig {
 
     @Value("${mongo.url}")
     private String DB_HOST;
@@ -16,11 +18,9 @@ public class MongoDB {
     public static final int DB_PORT = 27017;
     public static final String DB_NAME = "person-ms";
 
-    private static final MongoDB INSTANCE = new MongoDB();
-    private final Datastore datastore;
-
-
-    private MongoDB() {
+    @Bean
+    public Datastore datastore() {
+        System.out.println(DB_HOST);
         MongoClientOptions mongoOptions = MongoClientOptions.builder()
                 .socketTimeout(60000) // Wait 1m for a query to finish, https://jira.mongodb.org/browse/JAVA-1076
                 .connectTimeout(15000) // Try the initial connection for 15s, http://blog.mongolab.com/2013/10/do-you-want-a-timeout/
@@ -31,18 +31,12 @@ public class MongoDB {
         MongoClientURI uri = new MongoClientURI(DB_HOST);
         MongoClient mongoClient = new MongoClient(uri);
 
-        this.datastore = new Morphia().createDatastore(mongoClient, DB_NAME);
+        var datastore = new Morphia().createDatastore(mongoClient, DB_NAME);
         datastore.ensureIndexes();
         datastore.ensureCaps();
 
         System.out.println("Connection to database '" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME + "' initialized");
-    }
 
-    public static MongoDB instance() {
-        return INSTANCE;
-    }
-
-    public Datastore getDatabase() {
         return datastore;
     }
 }
